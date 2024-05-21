94aeefa9e1ecec4f02c1bb840be714d66d10371e
habib33-3 <habib.swi16@gmail.com>
2024-05-21 11:53:09 +0600

    optimzed code

--- src/components/MainTable/MainTable.tsx
+++ src/components/MainTable/MainTable.tsx
@@ -1,35 +1,43 @@
 import useJob from "@/hook/useJob";
 import { DataTable } from "./DataTable";
 import { columns } from "./Column";
+import { formatCurrency } from "@/lib/utils";
+import { useMemo } from "react";
 
 const MainTable = () => {
   const jobs = useJob();
 
-  const uniqueYears = Array.from(new Set(jobs.map((obj) => obj.work_year)));
+  const data = useMemo(() => {
+    // Get all unique years from the dataset
+    const uniqueYears = Array.from(new Set(jobs.map((obj) => obj.work_year)));
 
-  const jobsInYear = (year: number) =>
-    jobs.filter((job) => job.work_year === year);
+    // Filter the jobs for a certain year
+    const jobsInYear = (year: number) =>
+      jobs.filter((job) => job.work_year === year);
 
-  const averageSalary = (year: number) => {
-    const jobsForYear = jobsInYear(year);
-    const totalSalary = jobsForYear.reduce(
-      (acc, curr) => acc + curr.salary_in_usd,
-      0
-    );
-    const average = totalSalary / jobsForYear.length;
-    return average.toFixed(2);
-  };
-
-  const data = uniqueYears.map((year) => {
-    const jobsForYear = jobsInYear(year);
-    const jobNumber = jobsForYear.length;
-    const avgSalary = averageSalary(year);
-    return {
-      year: year,
-      totalJob: jobNumber,
-      averageSalary: avgSalary,
+    // Calculate the average salary for a certain year
+    const averageSalary = (year: number) => {
+      const jobsForYear = jobsInYear(year);
+      const totalSalary = jobsForYear.reduce(
+        (acc, curr) => acc + curr.salary_in_usd,
+        0
+      );
+      const average = totalSalary / jobsForYear.length;
+      return formatCurrency(average);
     };
-  });
+
+    // Map an array with objects containing year, total job count, and average salary
+    return uniqueYears.map((year) => {
+      const jobsForYear = jobsInYear(year);
+      const jobNumber = jobsForYear.length;
+      const avgSalary = averageSalary(year);
+      return {
+        year: year,
+        totalJob: jobNumber,
+        averageSalary: avgSalary,
+      };
+    });
+  }, [jobs]);
 
   return (
     <div>
